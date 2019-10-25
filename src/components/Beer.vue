ad582cd63f26c8a4db7758f892e27eb6f5101ac5
Alejandro Galviz <alegalviz@gmail.com>
2019-10-24 21:04:41 -0300

    Beers initial layout

---
+++ src/components/Beer.vue
@@ -0,0 +1,62 @@
+<template>
+  <sui-card class="beer">
+    <sui-card-content class="beer-header">
+      <h3 class="left floated name">{{beer.name}}</h3>
+      <sui-card-meta slot="right">{{year}}</sui-card-meta>
+    </sui-card-content>
+    <sui-card-content class="beer-body">
+      <sui-image
+      :src="beer.image_url"
+      size="mini"
+      class="image"
+      floated="left"
+    />
+    </sui-card-content>
+    <sui-card-content>
+      <span slot="right">
+        <sui-icon name="heart outline" /> {{ Math.floor(Math.random() * 30)}} likes
+      </span>
+      <sui-icon name="comment" /> {{ Math.floor(Math.random() * 10)}} comments
+    </sui-card-content>
+    <sui-card-content extra>
+      <sui-input
+        placeholder="Add Comment"
+        icon="heart outline"
+        icon-position="left"
+        transparent
+      />
+    </sui-card-content>
+  </sui-card>
+</template>
+<script>
+
+export default {
+  name: 'Beer',
+  props: {
+    beer: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    year () {
+      const first_brewed = this.beer.first_brewed.split('/');
+      return first_brewed[1]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.beer {
+  .name {
+    max-width: 80%;
+  }
+  &-body {
+    min-height: 200px;
+  }
+}
+</style>
---
+++ src/components/Beers.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ui four column stackable grid container">
+    <div
+      class="column stretched"
+      v-for="beer of beers"
+      :key="beer.id"
+    >
+      <Beer :beer="beer" class="ui segment" />
+    </div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Beer from '@/components/Beer.vue'
+
+export default {
+  name: 'Beers',
+  components: {
+    Beer
+  },
+  props: {
+    beers: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
