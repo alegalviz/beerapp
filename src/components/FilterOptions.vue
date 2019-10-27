37ca64073532890cc15e3d0397ac84b0cc8a8c88
Alejandro Galviz <alegalviz@gmail.com>
2019-10-26 22:55:59 -0300

    Filter sidebar

---
+++ src/components/FilterOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <h4 class="ui horizontal divider header">
+      <i class="filter icon"></i>
+      Filters
+    </h4>
+    <a class="active item">Beers!</a>
+    <a class="item">Log out</a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FilterOptions'
+}
+</script>
