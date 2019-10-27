8f3cc0c9b0d95b595c3b0a4f2a922db7e72dc4dc
Alejandro Galviz <alegalviz@gmail.com>
2019-10-26 22:56:46 -0300

    Beer routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BeerDetail from '../views/BeerDetail.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,14 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/beer/:beerId',
+        name: 'beer',
+        component: BeerDetail
+      }
+    ]
   }
 ]
 
